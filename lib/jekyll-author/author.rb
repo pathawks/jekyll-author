347c80454db45d32ecd9e0c48ddeabd18884bfa3
Pat Hawks <pat@pathawks.com>
2016-04-30 21:03:55 -0700

    Fix ABC error

--- lib/jekyll-author/author.rb
+++ lib/jekyll-author/author.rb
@@ -9,24 +9,34 @@ module JekyllAuthor
     # author - An author object or an author name string
     def initialize(site, author)
       @site = site
+      @author = author
 
-      author ||= site.config['author']
+      lookup_author
 
-      if author.is_a?(String) && site.config['authors'].is_a?(Array)
-        site.config['authors'].each do |a|
-          if a['name'] == author
-            author = a
+      if @author.is_a? Hash
+        self['name'] = @author['name']
+        self['email'] = @author['email']
+        self['uri'] = @author['uri']
+      elsif @author.is_a? String
+        self['name'] = @author
+      end
+    end
+
+    private
+
+    def default
+      @site.config['author']
+    end
+
+    def lookup_author
+      @author ||= default
+      if @author.is_a?(String) && @site.config['authors'].is_a?(Array)
+        @site.config['authors'].each do |a|
+          if a['name'] == @author
+            @author = a
             break
           end
         end
-      end
-
-      if author.is_a? Hash
-        self['name'] = author['name']
-        self['email'] = author['email']
-        self['uri'] = author['uri']
-      elsif author.is_a? String
-        self['name'] = author
       end
     end
   end
